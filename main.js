97744e52929b79cc0f6867afc4e8d4bac792c4b2
liquidgizmo <williamchensue@gmail.com>
2022-07-07 03:13:50 -0400

    First rps branch commit!

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
+let myScore = 0 
+let oppScore = 0 
+
+let swordButton = document.getElementById("rock")
+let shieldButton = document.getElementById("paper")
+let bowButton = document.getElementById("scissor")
+
+swordButton.addEventListener("click", function(){
+ playRound("rock")
+})
+shieldButton.addEventListener("click", function(){
+  playRound("paper")
+})
+bowButton.addEventListener("click", function(){
+  playRound("scissor")
+})
+
 function computerPlay() {
   let number = Math.floor(Math.random() * 3) + 1;
   let turn = "";
   if (number == 1) {
     turn = "rock";
   } else if (number == 2) {
-    
+
     turn = "paper";
   } else if (number == 3) {
     turn = "scissor";
@@ -13,38 +30,70 @@ function computerPlay() {
 }
 
 function singleRound(playerSelection, computerSelection) {
-  let result = "";
+  let result = null;
   playerSelection = playerSelection.toLowerCase()
   computerSelection = computerSelection.toLowerCase()
   if (playerSelection == "rock" && computerSelection == "rock") {
-    result = "The Match Is Tied!";
+    result = null;
   } else if (playerSelection == "rock" && computerSelection == "paper") {
-    result = "You Lose! Paper beats Rock";
+    result = false;
   } else if (playerSelection == "rock" && computerSelection == "scissor") {
-    result = "You Win! Rock beats Scissor";
+    result = true;
   } else if (playerSelection == "paper" && computerSelection == "rock") {
-    result = "You Win! Paper beats Rock";
+    result = true;
   } else if (playerSelection == "paper" && computerSelection == "paper") {
-    result = "The Match Is Tied!";
+    result = null;
   } else if (playerSelection == "paper" && computerSelection == "scissor") {
-    result = "You Lose! Scissor beats Paper";
+    result = false;
   } else if (playerSelection == "scissor" && computerSelection == "rock") {
-    result = "You Lose! Rock beats Scissor";
+    result = false;
   } else if (playerSelection == "scissor" && computerSelection == "paper") {
-    result = "You Win! Scissor beats Paper";
+    result = true;
   } else if (playerSelection == "scissor" && computerSelection == "scissor") {
-    result = "The Match Is Tied!";
+    result = null;
   }
  return result 
 }
 
-function game() {
-for (let i = 0; i < 5; i++) {
-    let userInput = prompt("Please Make A Selection")
-    let computerInput = computerPlay() 
+function playRound(userInput) {
+//for (let i = 0; i < 5; i++) {
+ //   let userInput = prompt("Please Make A Selection")
+    let computerInput = computerPlay()
+
   let result =  singleRound(userInput, computerInput)
-  console.log(result)
- }
+  
+  if (result == true ) {
+    myScore++ 
+  }
+
+else if(result == false) {
+  oppScore++
 }
-game()
+
+
+
+document.getElementById("myscore").innerHTML = `My Score: ${myScore}`;
+document.getElementById("oppscore").innerHTML = `Opponent's Score: ${oppScore}`;
+
+if (myScore >= 5 ) {
+  myScore = 0 
+  oppScore = 0
+  document.getElementById("myscore").innerHTML = `My Score: ${myScore}`;
+document.getElementById("oppscore").innerHTML = `Opponent's Score: ${oppScore}`;
+
+  alert ("You Win!")
+}
+
+else if (oppScore >= 5) {
+  myScore = 0 
+  oppScore = 0
+  document.getElementById("myscore").innerHTML = `My Score: ${myScore}`;
+document.getElementById("oppscore").innerHTML = `Opponent's Score: ${oppScore}`;
+
+  alert ("Opponent Wins!")
+}
+
+// }
+}
+//playRound()
 
